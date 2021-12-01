f60a1d872ee1a611b4a6ed2585ba7e94d1620107
jagaru1998 <95221780+jagaru1998@users.noreply.github.com>
2021-12-01 15:25:59 +0100

    add props: persistent-hint and rules

--- src/components/ImageUploader.vue
+++ src/components/ImageUploader.vue
@@ -15,6 +15,8 @@
       :clearable="clearable"
       :color="color"
       :dark="dark"
+      :persistent-hint="persistentHint"
+      :rules="rules"
       :hint="hint"
       :loading="loading"
       :label="label"
@@ -167,6 +169,17 @@ export default {
       type: Boolean,
       default: true,
     },
+    
+    persistentHint: {
+      type: Boolean,
+      default: false,
+    },
+    
+    rules:{
+      type: Array,
+      default: [],
+    }
+    
     /**
      * Applies specified color to the control - it can be the name of material color (for example success or purple) or css color (#033 or rgba(255, 0, 0, 0.5)). You can find list of built in classes on the colors page.
      * @type {String}
