0871a2ca0cfbc57e356f3f1ef9969216fef3b363
jagaru1998 <95221780+jagaru1998@users.noreply.github.com>
2021-12-01 15:44:13 +0100

    fix

--- src/components/ImageUploader.vue
+++ src/components/ImageUploader.vue
@@ -178,7 +178,7 @@ export default {
     rules:{
       type: Array,
       default: [],
-    }
+    },
     
     /**
      * Applies specified color to the control - it can be the name of material color (for example success or purple) or css color (#033 or rgba(255, 0, 0, 0.5)). You can find list of built in classes on the colors page.
